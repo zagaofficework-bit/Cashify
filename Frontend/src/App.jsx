253450b292e64d5fe72ef127dea2b20f697a0493
zagaofficework-bit <zagaofficework@gmail.com>
2026-03-05 20:40:12 -0800

    new files added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react'
 import Home from './UI/pages/Home'
 import Login from './UI/pages/Login'
@@ -19,3 +20,44 @@ const App = () => {
 }
 
 export default App
+=======
+import { BrowserRouter, Routes, Route, ServerRouter } from "react-router-dom";
+import { Signup } from "./UI/pages/signup";
+import OtpGeneration from "./UI/pages/OtpGeneration";
+import SlidingAnimation from "./UI/components/SlidingAnimation";
+import ServiceCard from "./UI/components/ServiceCard";
+import ServicesSection from "./UI/components/ServiceSection";
+import SellOldDevice from "./UI/components/SellOldDevice";
+
+import StoreSection from "./UI/components/StoreSection";
+import ArticleSection from "./UI/components/ArticleSection";
+import DealComponent from "./UI/components/DealComponent";
+import TrendingSection from "./UI/components/TrendingSection";
+import Recents from "./UI/components/Recents";
+import Footer from "./UI/components/Footer";
+
+function App() {
+  return (
+    <>
+     {/* // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Signup />} />
+    //     <Route path="/otp" element={<OtpGeneration />} />
+    //   </Routes>
+    // </BrowserRouter> */}
+    <SlidingAnimation/>
+    <ServicesSection/>
+    <SellOldDevice/>
+    <StoreSection/>
+    <ArticleSection/>
+    <DealComponent/>
+    <TrendingSection/>
+    <Recents/>
+    <Footer/>
+    </>
+   
+  );
+}
+
+export default App;
+>>>>>>> a4a14d0 (new files added)
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,5 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+<<<<<<< HEAD
   plugins: [react(),tailwindcss()],
+=======
+  plugins: [react(),
+    tailwindcss()
+  ],
+>>>>>>> a4a14d0 (new files added)
 })
