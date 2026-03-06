f75924b8b91817c5e4c71cb511d99f2c55bda2b1
zagaofficework-bit <zagaofficework@gmail.com>
2026-03-05 22:26:45 -0800

    ..

--- Frontend/src/UI/pages/Home.jsx
+++ Frontend/src/UI/pages/Home.jsx
@@ -31,7 +31,7 @@ export default function Home(){
       <ServicesSection/>
     <SellOldDevice/>
 
-    <BuyRefurbishedDevices/>
+    <BuyRefurbisgithedDevices/>
     <RefurbishedLaptops/>
     <StoreSection/>
      
