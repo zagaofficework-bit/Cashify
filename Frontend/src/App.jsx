675f6656e35a1a78124db9e173cbf33381f70d87
zagaofficework-bit <zagaofficework@gmail.com>
2026-03-05 21:59:17 -0800

    add& edit files

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,59 +1,25 @@
-import React from 'react'
-import Home from './UI/pages/Home'
-import Login from './UI/pages/Login'
-import Header from './UI/components/NavBar'
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-
-const App = () => {
-  return (
-    <div>
-      <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/login" element={<Login/>}></Route>
-      </Routes>
-      </BrowserRouter>
-      
-    </div>
-  )
-}
-
-export default App
-import { BrowserRouter, Routes, Route, ServerRouter } from "react-router-dom";
-import { Signup } from "./UI/pages/signup";
-import OtpGeneration from "./UI/pages/OtpGeneration";
-import SlidingAnimation from "./UI/components/SlidingAnimation";
-import ServiceCard from "./UI/components/ServiceCard";
-import ServicesSection from "./UI/components/ServiceSection";
-import SellOldDevice from "./UI/components/SellOldDevice";
-
-import StoreSection from "./UI/components/StoreSection";
-import ArticleSection from "./UI/components/ArticleSection";
-import DealComponent from "./UI/components/DealComponent";
-import TrendingSection from "./UI/components/TrendingSection";
-import Recents from "./UI/components/Recents";
-import Footer from "./UI/components/Footer";
+import Brands from "./UI/components/Brands";
+import Home from "./UI/pages/Home";
 
 function App() {
   return (
     <>
+     {/* <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+      </Routes>
+      </BrowserRouter> */}
      {/* // <BrowserRouter>
     //   <Routes>
     //     <Route path="/" element={<Signup />} />
     //     <Route path="/otp" element={<OtpGeneration />} />
     //   </Routes>
     // </BrowserRouter> */}
-    <SlidingAnimation/>
-    <ServicesSection/>
-    <SellOldDevice/>
-    <StoreSection/>
-    <ArticleSection/>
-    <DealComponent/>
-    <TrendingSection/>
-    <Recents/>
-    <Footer/>
+    <Brands/>
     </>
    
   );
 }
 
+export default App;
--- Frontend/src/UI/pages/Home.jsx
+++ Frontend/src/UI/pages/Home.jsx
@@ -6,18 +6,50 @@ import Feedback from "../components/Feedback";
 import FAQ from "../components/FAQ";
 import DownloadAppBanner from "../components/DownloadAppBanner";
 import Info from "../components/Info";
+// import { BrowserRouter, Routes, Route, ServerRouter } from "react-router-dom";
+// import { Signup } from "./UI/pages/signup";
+// import OtpGeneration from "./UI/pages/OtpGeneration";
+import SlidingAnimation from "../components/SlidingAnimation";
+import ServiceCard from "../components/ServiceCard";
+import ServicesSection from "../components/ServiceSection";
+import SellOldDevice from "../components/SellOldDevice";
+
+import StoreSection from "../components/StoreSection";
+import ArticleSection from "../components/ArticleSection";
+import DealComponent from "../components/DealComponent";
+import TrendingSection from "../components/TrendingSection";
+import Recents from "../components/Recents";
+import Footer from "../components/Footer";
+
 
 
 export default function Home(){
     return (<>
     <NavBar/>
     <NavMenu/>
+     <SlidingAnimation/>
+      <ServicesSection/>
+    <SellOldDevice/>
+
     <BuyRefurbishedDevices/>
     <RefurbishedLaptops/>
+    <StoreSection/>
+     
     <Feedback/>
+    <ArticleSection/>
+    <DealComponent/>
     <FAQ/>
+     <TrendingSection/>
+      <Recents/>
     <DownloadAppBanner/>
     <Info/>
+   
+   
+    
+   
+   
+   
+    <Footer/>
     </>)
 }
 
