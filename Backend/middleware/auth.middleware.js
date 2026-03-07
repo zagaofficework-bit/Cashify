4739a65688ffb24a11ec44102c5b7dae4061cc37
zagaofficework-bit <zagaofficework@gmail.com>
2026-03-07 11:04:41 +0530

    OK Commit

--- Backend/middleware/auth.middleware.js
+++ Backend/middleware/auth.middleware.js
@@ -1,48 +1,5 @@
 const userModel = require("../models/user.model")
 const jwt = require("jsonwebtoken")
-<<<<<<< HEAD
-const tokenBlackListModel = require("../models/blackList.model")
-
-
-
-async function authMiddleware(req, res, next) {
-
-    const token = req.cookies.token || req.headers.authorization?.split(" ")[ 1 ]
-
-    if (!token) {
-        return res.status(401).json({
-            message: "Unauthorized access, token is missing"
-        })
-    }
-
-    const isBlacklisted = await tokenBlackListModel.findOne({ token })
-
-    if (isBlacklisted) {
-        return res.status(401).json({
-            message: "Unauthorized access, token is invalid"
-        })
-    }
-
-    try {
-
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-
-        const user = await userModel.findById(decoded.userId)
-
-        req.user = user
-
-        return next()
-
-    } catch (err) {
-        return res.status(401).json({
-            message: "Unauthorized access, token is invalid"
-        })
-    }
-}
-
-module.exports = {
-    authMiddleware
-=======
 const redisClient = require("../config/redis.client");
 
 const BLACKLIST_KEY = (token) => `blacklist:${token}`;
@@ -115,5 +72,4 @@ function authorize(...roles) {
 module.exports = {
     authMiddleware,
     authorize
->>>>>>> 442de8276e9e704f159e456a5893934fa209054a
 }
